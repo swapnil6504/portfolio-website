b357258177362f971fd8e1ef0eb992c44b94aea3
swapnil6504 <swapnilchhibber6504@gmail.com>
2024-02-11 16:56:08 +0530

    change

--- script/home-page.js
+++ script/home-page.js
@@ -4,3 +4,27 @@ function scrollToSection(sectionId) {
     section.scrollIntoView({ behavior: "smooth" });
   }
 }
+
+function toggleTheme() {
+  const body = document.body;
+  body.classList.toggle("dark-mode");
+  const themeStyle = document.getElementById("theme-style");
+  const isDarkMode = body.classList.contains("dark-mode");
+
+  const iconToggle = document.getElementById("icon-toggle");
+  const sunIcon = iconToggle.querySelector(".lucide-sun");
+  const moonIcon = iconToggle.querySelector(".lucide-moon");
+  sunIcon.style.display = isDarkMode ? "none" : "inline-block";
+  moonIcon.style.display = isDarkMode ? "inline-block" : "none";
+
+  localStorage.setItem("dark-mode", isDarkMode);
+
+  themeStyle.href = `../css/home-page${isDarkMode ? "-dark" : ""}.css`;
+}
+
+const prefersDarkMode = localStorage.getItem("dark-mode") === "true";
+
+if (prefersDarkMode) {
+  document.body.classList.add("dark-mode");
+  toggleTheme();
+}
